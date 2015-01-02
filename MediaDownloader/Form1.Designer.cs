2077f0280f63e1fd2d2543c67be445eb9175ad68
Reslof <lflore26@uic.edu>
2015-01-02 16:06:39 -0600

    Initial commit

---
+++ MediaDownloader/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace MediaDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.videoOrigButton = new System.Windows.Forms.RadioButton();
+            this.radioMP3Button = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveLocationBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 68F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 257F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(373, 410);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.videoOrigButton);
+            this.groupBox1.Controls.Add(this.radioMP3Button);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.saveLocationBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 62);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "&Browse...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // videoOrigButton
+            // 
+            this.videoOrigButton.AutoSize = true;
+            this.videoOrigButton.Location = new System.Drawing.Point(178, 39);
+            this.videoOrigButton.Name = "videoOrigButton";
+            this.videoOrigButton.Size = new System.Drawing.Size(94, 17);
+            this.videoOrigButton.TabIndex = 4;
+            this.videoOrigButton.TabStop = true;
+            this.videoOrigButton.Text = "&Video (original)";
+            this.videoOrigButton.UseVisualStyleBackColor = true;
+            this.videoOrigButton.CheckedChanged += new System.EventHandler(this.videoOrigButton_CheckedChanged);
+            // 
+            // radioMP3Button
+            // 
+            this.radioMP3Button.AutoSize = true;
+            this.radioMP3Button.Checked = true;
+            this.radioMP3Button.Location = new System.Drawing.Point(91, 39);
+            this.radioMP3Button.Name = "radioMP3Button";
+            this.radioMP3Button.Size = new System.Drawing.Size(81, 17);
+            this.radioMP3Button.TabIndex = 3;
+            this.radioMP3Button.TabStop = true;
+            this.radioMP3Button.Text = "&Audio (mp3)";
+            this.radioMP3Button.UseVisualStyleBackColor = true;
+            this.radioMP3Button.CheckedChanged += new System.EventHandler(this.radioMP3Button_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Format:";
+            // 
+            // saveLocationBox
+            // 
+            this.saveLocationBox.Location = new System.Drawing.Point(91, 13);
+            this.saveLocationBox.Name = "saveLocationBox";
+            this.saveLocationBox.Size = new System.Drawing.Size(186, 20);
+            this.saveLocationBox.TabIndex = 1;
+            this.saveLocationBox.TextChanged += new System.EventHandler(this.saveLocationBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Save Location:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.outputBox);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(367, 251);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(3, 16);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(361, 232);
+            this.outputBox.TabIndex = 6;
+            this.outputBox.TabStop = false;
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(367, 45);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Input";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.82271F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.17728F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.urlTextBox, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(361, 26);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Video URL: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.AcceptsTab = true;
+            this.urlTextBox.Location = new System.Drawing.Point(88, 3);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(270, 20);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlBox_TextChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.helpButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(121, 379);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(249, 28);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "&Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(165, 3);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 7;
+            this.helpButton.Text = "&Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(373, 410);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(389, 449);
+            this.MinimumSize = new System.Drawing.Size(389, 449);
+            this.Name = "Form1";
+            this.Text = "Media Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox saveLocationBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton videoOrigButton;
+        private System.Windows.Forms.RadioButton radioMP3Button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button helpButton;
+    }
+}
+
---
+++ MediaDownloader/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void urlBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveLocationBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioMP3Button_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void videoOrigButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+            Application.UseWaitCursor = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //sets download path read only
+            saveLocationBox.ReadOnly = true;
+            urlTextBox.ReadOnly = true;
+
+            //gets radio option
+            int option = 0;
+
+            if (radioMP3Button.Checked)
+            {
+                option = 0;
+            }
+            else if (videoOrigButton.Checked)
+            {
+                option = 1;
+            }
+            //room for more options
+
+            string url = urlTextBox.Text;
+            if (System.Uri.IsWellFormedUriString(url, System.UriKind.Absolute))
+            {
+                outputBox.Clear();
+                LaunchYoutubedl(url, option);
+                urlTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Invalid URL. Please enter a valid URL. For more help, click the \"Help\" button.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            //sets download location to Desktop as a default
+            saveLocationBox.AppendText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        }
+        void LaunchYoutubedl(string url, int option)
+        {
+            string downloadLocation = saveLocationBox.Text;
+            string formatOptions = "";
+            var proc = new Process();
+            if(option == 0){
+
+                formatOptions = " -x --audio-quality 0 --prefer-ffmpeg --audio-format mp3";
+            }
+            else if (option == 1)
+            {
+                formatOptions = "";
+            }
+           
+            // set up output redirection and process parameters
+            proc.StartInfo.WorkingDirectory = downloadLocation;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.EnableRaisingEvents = true;
+            proc.StartInfo.CreateNoWindow = true;
+
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.FileName = "youtube-dl.exe";
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.Arguments = " -o \"%(title)s.%(ext)s\"" +
+                                  formatOptions +
+                                  " --restrict-filenames " +
+                                  url;
+
+            // Set the data received handlers
+            proc.ErrorDataReceived += proc_DataReceived;
+            proc.OutputDataReceived += proc_DataReceived;
+
+            // Configure the process exited event
+            proc.Exited += new EventHandler(ProcExited);
+
+            proc.Start();
+            proc.BeginErrorReadLine();
+            proc.BeginOutputReadLine();
+
+        }
+
+        delegate void AppendTextDelegate(string text);
+
+        // Thread-safe method of appending text to the console box
+        private void AppendText(string text)
+        {
+            // Use a delegate if called from a different thread,
+            // else just append the text directly
+            if (this.outputBox.InvokeRequired)
+            {
+                // Application crashes when this line is executed
+                outputBox.Invoke(new AppendTextDelegate(this.AppendText), new object[] { text + Environment.NewLine }); //adds a newline at the end
+            }
+            else
+            {
+                this.outputBox.AppendText(text);
+                
+            }
+        }
+        
+        // Handle the date received by the console process
+        void proc_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                this.AppendText(e.Data);
+
+            }
+        }
+        /// <summary>
+        /// Actions to take when console process completes
+        /// </summary>
+        private void ProcExited(object sender, System.EventArgs e)
+        {
+            Process proc = (Process)sender;
+
+            // Wait a short while to allow all console output to be processed and appended
+            // before appending the success/fail message.
+            Thread.Sleep(40);
+
+            if (proc.ExitCode == 0)
+            {
+                this.AppendText(Environment.NewLine + "Success.");
+                MessageBox.Show("Operation completed successfully.");
+                
+            }
+            else
+            {
+                this.AppendText(Environment.NewLine + "Failed.");
+                MessageBox.Show("Operation failed.");
+
+            }    
+            
+            //thread safe way to update the UI
+            this.InvokeEx(f => Application.UseWaitCursor = false);
+            this.InvokeEx(f => urlTextBox.ReadOnly = false);
+            this.InvokeEx(f => saveLocationBox.ReadOnly = false);
+
+            proc.Close();
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+            "If you are having trouble downloading audio from a specific site, be sure that the website is supported by youtube-dl." +
+            "\nSupported sites: https://rg3.github.io/youtube-dl/supportedsites.html" +
+            "\n\nIcons made by Freepik. Flaticon Creative Commons BY 3.0",
+            "Help",
+            MessageBoxButtons.OKCancel,
+            MessageBoxIcon.Information,
+            MessageBoxDefaultButton.Button1
+            );
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            saveLocationBox.Clear();
+            saveLocationBox.AppendText(fbd.SelectedPath);
+            saveLocationBox.ReadOnly = true;
+        }
+    }
+}
+//thread safe way of accessing UI elements. Thanks StackOverflow
+public static class ISynchronizeInvokeExtensions
+{
+    public static void InvokeEx<T>(this T @this, Action<T> action) where T : ISynchronizeInvoke
+    {
+        if (@this.InvokeRequired)
+        {
+            @this.Invoke(action, new object[] { @this });
+        }
+        else
+        {
+            action(@this);
+        }
+    }
+}
